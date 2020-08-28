a305564b7f4b65d385e5703616f20a157ce9a328
makmakkerti <makmakkerti007@gmail.com>
2020-08-28 16:35:52 +0300

    Recipe model added, recipe list added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Shwarma Recipe 1', 'Shwarma Recipe description', 'https://www.kitchensanctuary.com/wp-content/uploads/2015/02/Chicken-Shawarma-square-FS-57.jpg'),
+    new Recipe('Falafel Recipe', 'Falafel prepare from nut!', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/07/Falafel-7-1.jpg'),
+    new Recipe('Thina Recipe', 'Thina from melted seeds recipe', 'https://i.ytimg.com/vi/dh2LPPXMo0A/maxresdefault.jpg')
+  ];
 
   constructor() { }
 
